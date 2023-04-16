cd04be76f6c7bb2669bff0bfafef773b60b6c676
Gustavo Victor <gguvictor999@outlook.com>
2023-04-16 16:37:36 +0000

    Update 15 files

---
+++ src/components/Attribution/index.jsx
@@ -0,0 +1,22 @@
+import "./styles.scss";
+
+function Attribution() {
+  return (
+    <div className="attribution">
+      <div>
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+      </div>
+      <div>
+        Coded by{" "}
+        <a href="https://github.com/Gustavo-Victor/" target="_blank">
+          Gustavo Victor
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Attribution;
